0930cca834c448289ee2a6ceb734a267e0d7214d
Stefano <zhuhong.jin@edu.unifi.it>
2024-09-28 17:20:28 +0200

    Add interface for user.

---
+++ ToDoInterface.h
@@ -0,0 +1,28 @@
+//
+// Created by sjin0 on 2024/9/28.
+//
+
+#ifndef ELABORATOJIN_TODOINTERFACE_H
+#define ELABORATOJIN_TODOINTERFACE_H
+
+#include "Todolist.h"
+
+class ToDoInterface {
+private:
+    Todolist& list;
+
+public:
+    //constructor
+    ToDoInterface(Todolist &list);
+
+    //method for interface
+    void printMenu() const;
+    void add();
+    void remove();
+    void showActivity();
+    void saveList();
+    void loadList();
+};
+
+
+#endif //ELABORATOJIN_TODOINTERFACE_H
--- main.cpp
+++ main.cpp
@@ -5,25 +5,44 @@
 
 #include <iostream>
 #include "Todolist.h"
+#include "ToDoInterface.h"
 
 int main() {
     Todolist list;
+    ToDoInterface interface(list);
 
-    //add activities
-    list.addActivity(Activity("Fare la spesa", "comprare frutta e verdura"));
-    list.addActivity(Activity("Studiare C++", "completare il capitolo sulla gestione dei file"));
+    bool exit = false;
 
-    //save list on disco
-    list.saveOnDisc("todolist.txt");
+    while (!exit) {
+        interface.printMenu();
+        int choice;
+        cin >> choice;
+        cin.ignore();  // Per eliminare il newline rimanente nel buffer di input
 
-    //print list
-    cout << "Activities list: " << endl;
-    list.print();
-
-    //load from disc and print
-    cout << "\nLoading list from disc..." << endl;
-    list.downloadFromDisc("todolist.txt");
-    list.print();
-
+        switch (choice) {
+            case 1:
+                interface.add();
+                break;
+            case 2:
+                interface.remove();
+                break;
+            case 3:
+                interface.showActivity();
+                break;
+            case 4:
+                interface.saveList();
+                break;
+            case 5:
+                interface.loadList();
+                break;
+            case 6:
+                exit = true;
+                cout << "Exiting the program..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+                break;
+        }
+    }
     return 0;
 }
